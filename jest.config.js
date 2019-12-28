69bde250838d4fc2e5f884822ed44339c47db4fd
upcwangying <upcwangying@126.com>
2019-12-28 15:46:13 +0800

    fix: .travis.yml

--- jest.config.js
+++ jest.config.js
@@ -22,6 +22,7 @@ module.exports = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: null,
+  collectCoverageFrom: ['**/*.[jt]s?(x)', '!**/node_modules/**', '!**/dist/**'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
